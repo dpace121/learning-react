614b1a1e7c3ba4f06349f6d951d1523cc9ee4a5b
dpace121 <kayasthadipesh99@gmail.com>
2025-12-29 21:26:59 +0545

    api-call done

---
+++ 11-api_call/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [data,setData] = useState([])
+
+  const getData =  async () =>{
+
+    // const response = await fetch('https://jsonplaceholder.typicode.com/todos/')  
+
+    // const data = await response.json()
+
+    // console.log(data)
+    const response = await axios.get("https://picsum.photos/v2/list")
+
+    setData(response.data);
+
+
+  }
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem, idx){
+          return <h3>Hello {elem.author}</h3>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
